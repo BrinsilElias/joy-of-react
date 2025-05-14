77d0848bced441e64bc3f6148ddba4916864f953
Brinsil Elias <brinsilelias0592@gmail.com>
2025-05-14 07:41:32 +0530

    feat: Excercise 06 and Excercise 07

---
+++ module-01/exercise-07/src/components/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import VisuallyHidden from "./VisuallyHidden";
+
+function Friend({ name, isOnline }) {
+  return (
+    <li className="friend">
+      {isOnline && <div className="green-dot" />}
+      {name}
+      {isOnline && <VisuallyHidden>Online</VisuallyHidden>}
+    </li>
+  );
+}
+
+function App() {
+  return (
+    <ul className="friend-list">
+      <Friend name="Andrew" isOnline={false} />
+      <Friend name="Beatrice" isOnline={true} />
+      <Friend name="Chen" isOnline={true} />
+    </ul>
+  );
+}
+
+export default App;
